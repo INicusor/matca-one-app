fc05b2938abd405f43495d5b9cce85408314f200
INicusor <49711918+INicusor@users.noreply.github.com>
2026-06-09 15:56:55 +0300

    feat: add jurnal view

---
+++ views/jurnal.php
@@ -0,0 +1,5 @@
+<section id="view-jurnal" class="view-section">
+    <div class="page-container">
+        <div class="dashboard-grid">
+            <div>
+                <h2>📔 Jurnal Inspecții</h2>
